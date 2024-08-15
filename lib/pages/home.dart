17d225d444337b70c79e7f34163caf5c84dbe786
Daniel <danielbolaji99@gmail.com>
2024-08-15 10:50:51 +0100

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.only(bottomRight: Radius.circular(250))),
+          toolbarHeight: kToolbarHeight + 30,
+          title: const Text(
+            'ConvertX',
+            style: TextStyle(
+                color: Color(0xFFE3E8EE),
+                fontSize: 35,
+                fontFamily: 'Kurale_Regular',
+                fontWeight: FontWeight.w700),
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: const Color(0xFF3F82B0)),
+      backgroundColor: const Color(0xFFE3E8EE),
+      drawer: Drawer(
+        backgroundColor: const Color(0xFF3F82B0),
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Color(0xFF3F82B0)),
+              child: Text('ConvertX'),
+            ),
+            ListTile(
+              title: const Text('Nigeria Naira'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Nigeria Naira'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/welcomescreen.dart
@@ -0,0 +1,25 @@
+import 'package:currency_converter/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class Welcomescreen extends StatelessWidget {
+  const Welcomescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
+    });
+    return Scaffold(
+      backgroundColor: const Color(0xFF3F82B0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/images/logo1.png')],
+        ),
+      ),
+    );
+  }
+}
